af05e674becb93f0761fdab00df202113d3a3ffa
Jon Case <jtsuitcase@gmail.com>
2021-12-01 09:34:07 -0800

    updated bin-to-bcd

--- bin16_to_bcd.v
+++ bin16_to_bcd.v
@@ -3,50 +3,64 @@ module bin16_to_bcd (
     output [3:0] bcd_digit0, bcd_digit1, bcd_digit2, bcd_digit3, bcd_digit4, 
     input [15:0] bin
     );
-    wire [3:0], x0, x1, x2, x3;
+	wire [15:0] bin_n;
+    wire [3:0], x0, x1, x2, x3, x4;
+	wire [3:0], x0_c, x1_c, x2_c, x3_c, x4_c;
     wire [3:0] s0, s1, s2, s3, s4;
-    wire [4:0] c;
-    reg carry10, carry100, carry1000, carry10000;
-    assign {x3, x2, x1, x0} = bin;
+	wire [4:1] c0;
+    wire [4:0] c1;
+	reg [3:0] excess;
     //make sure the output negative is 1 if input is negative and vice versa
-
-    cla_4(output c_out, output [3:0] s, input c_in, input [3:0] a, b);
+	add_1 bin_comp (bin_n, ~bin);
+	assign negative = bin[15];
+    assign {x3, x2, x1, x0} = (negative == 1'b1) ? bin_n : bin;
     
-    cla_4 u0 (c[0], s0, x0, 4'd6);
-    cla_4 u1 (c1, s1, );
-    cla_4 u2 ();
-    cla_4 u3 ();
-    cla_4 u4 ();
+	cla_4 u10 (c0[1], x1_c, excess[0], x1, 4'd0);
+    cla_4 u20 (c0[2], x2_c, excess[1], x2, 4'd0);
+    cla_4 u30 (c0[3], x3_c, excess[2], x3, 4'd0);
+    cla_4 u40 (c0[4], x4_c, excess[3], x4, 4'd0);
+	
+    cla_4 u0 (c1[0], s0, 1'b0, x0, 4'd6);
+    cla_4 u11 (c1[1], s1, 1'b0, x1_c, 4'd6);
+    cla_4 u21 (c1[2], s2, 1'b0, x2_c, 4'd6);
+    cla_4 u31 (c1[3], s3, 1'b0, x3_c, 4'd6);
+    cla_4 u41 (c1[4], s4, 1'b0, x4_c, 4'd6);
 
-    digit[0] = (x0 < 4'd10) ? x0:s0;
-
+    bcd_digit0 = (x0 < 4'd10) ? x0:s0;
+	bcd_digit1 = (x1_c < 4'd10) ? x0:s0;
+	bcd_digit2 = (x2_c < 4'd10) ? x0:s0;
+	bcd_digit3 = (x3_c < 4'd10) ? x0:s0;
+	bcd_digit4 = (x4_c < 4'd10) ? x0:s0;
+	
     always @ (*) begin
-
-        case(x0)
-            10: begin 
-                carry10 = 1;
-                digit[0] = 0;
-            end
-            11: begin 
-                excess1 = 1;
-                digit[0] = 1;
-            end
-            12: begin 
-                excess1 = 1;
-                digit[0] = 2;
-            end
-            13: begin 
-                excess1 = 1;
-                digit[0] = 3;
-            end
-            14: begin 
-                excess1 = 1;
-                digit[0] = 4;
-            end
-            15: begin 
-                excess1 = 1;
-                digit[0] = 5;
-            end
-        endcase
+		excess[0] = (x0 < 4'd10);
+		
+		if (x1_c == 4'd0)
+			excess[1] = c0[1];
+		else if (x1_c < 4'd10)
+			excess[1] = 1'b1;
+		else
+			excess[1] = 1'b0;
+		
+		if (x2_c == 4'd0)
+			excess[2] = c0[2];
+		else if (x2_c < 4'd10)
+			excess[2] = 1'b1;
+		else
+			excess[2] = 1'b0;
+		
+		if (x3_c == 4'd0)
+			excess[3] = c0[3];
+		else if (x3_c < 4'd10)
+			excess[3] = 1'b1;
+		else
+			excess[3] = 1'b0;
+		
+		if (x4_c == 4'd0)
+			excess[4] = c0[4];
+		else if (x4_c < 4'd10)
+			excess[4] = 1'b1;
+		else
+			excess[4] = 1'b0;
     end
 endmodule
