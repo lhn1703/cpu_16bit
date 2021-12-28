029b8b652461dc87422ec38de27435330aead65a
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2021-12-28 14:05:12 -0800

    Update cpu_16bit.v

--- cpu_16bit.v
+++ cpu_16bit.v
@@ -28,20 +28,5 @@ module cpu_16bit (output [15:0] result_reg, input [15:0] initial_input, input cl
 	
 	
 	// Pipelining
-	reg [31:0] IF_ID_pipe;
-	reg [105:0] ID_EX_pipe;
-	reg [86:0] EX_MEM_pipe;
-	wire IF_ID_write, IF_ID_async_nop
-	
-	always @(posedge IF_ID_AsyncNOP) begin
-			IF_ID_pipe <= 0;
-	end
-	
-	always @(posedge clk) begin
-		if (IF_ID_write & ~IF_ID_async_nop)
-			IF_ID_pipe <= {IF_pc_plus_1, IF_instruction};
-		ID_EX_pipe <= {ID_WB_control, ID_MEM_control, ID_EX_control
-	end
-
 
 endmodule
