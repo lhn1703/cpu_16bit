fb151e14a980dcb13cf6ca5598fa942416700fca
Jon Case <jtsuitcase@gmail.com>
2021-12-27 16:48:20 -0800

    Update cpu_16bit.v

--- cpu_16bit.v
+++ cpu_16bit.v
@@ -1,6 +1,33 @@
 module cpu_16bit (output [15:0] result_reg, input [15:0] initial_input, input clk, pc_reset);
     
-	//Pipelining
+	// IF
+	
+	
+	// IF/ID
+	
+	
+	// ID
+	
+	
+	// ID/EX
+	
+	
+	// EX
+	
+	
+	// EX/MEM
+	
+	
+	// MEM
+	
+	
+	// MEM/WB
+	
+	
+	// WB
+	
+	
+	// Pipelining
 	reg [31:0] IF_ID_pipe;
 	reg [105:0] ID_EX_pipe;
 	reg [86:0] EX_MEM_pipe;
