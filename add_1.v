25798e76c4d4d042a5ac82da6b56deefdb1937b1
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2022-01-05 09:33:51 -0800

    reverted back to explicit alu logic

--- add_1.v
+++ add_1.v
@@ -1,10 +1,10 @@
 
 module add_1(output [15:0] out, input [15:0] in);
-	assign out = in+1;
-	/*
+	//assign out = in+1;
+	
 	wire [15:0] c_in;
 	assign c_in[0] = 1'b1;
 	assign c_in[15:1] = in[14:0] & c_in[14:0];
 	assign out[15:0] = in[15:0] ^ c_in[15:0];
-	*/
+	
 endmodule
